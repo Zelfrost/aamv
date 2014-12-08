b5932af5ac9b8e851e51d45f599cc7ccdda32af0
Zelfrost <deconinck.damien@gmail.com>
2014-12-08 21:11:48 +0100

    Add config

---
+++ src/Aamv/Bundle/SiteBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Aamv\Bundle\SiteBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('aamv');
+
+        return $treeBuilder;
+    }
+}
