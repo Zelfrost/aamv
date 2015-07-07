5cf9d0d5098377f31382d06bd8e74dddc7b77928
GuillaumeCo <copinguillaume@hotmail.fr>
2015-07-07 18:43:16 +0200

    Registration

--- src/Aamv/Bundle/SiteBundle/Form/Type/RegistrationFormType.php
+++ src/Aamv/Bundle/SiteBundle/Form/Type/RegistrationFormType.php
@@ -2,6 +2,24 @@
 
 namespace Aamv\Bundle\SiteBundle\Form\Type;
 
-class RegistrationFormType {
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
 
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')
+            ->add('firstname');
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return 'aamv_user_registration';
+    }
 }
