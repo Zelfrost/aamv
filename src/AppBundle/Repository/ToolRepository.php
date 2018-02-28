cfd3831b085b881335102edfa1fe61a108c15bd4
Deconinck Damien <ddeconinck@auchan.net>
2018-02-28 19:36:11 +0100

    Fix categories order

--- src/AppBundle/Repository/ToolRepository.php
+++ src/AppBundle/Repository/ToolRepository.php
@@ -9,17 +9,19 @@ class ToolRepository extends EntityRepository
 {
     public function findFiles(string $type, Category $category = null, bool $inversedOrder = false)
     {
-        return $this->createQueryBuilder('t')
-            ->where('t.category = :category')
-            ->andWhere('t.type = :type')
+        $builder = $this->createQueryBuilder('t')
+            ->where('t.type = :type')
             ->setParameter('type', $type)
-            ->setParameter('category', $category)
             ->orderBy(
                 null === $category|| 'name' === $category->getOrderField() ? 't.name' : 't.date',
                 null === $category|| 'name' === $category->getOrderField() ? 'ASC' : 'DESC'
             )
-            ->getQuery()
-            ->getResult()
         ;
+
+        if (null !== $category) {
+            $builder->andWhere('t.category = :category')->setParameter('category', $category);
+        }
+
+        return $builder->getQuery()->getResult();
     }
 }
