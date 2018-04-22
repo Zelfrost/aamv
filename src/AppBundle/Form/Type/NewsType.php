41b670ad82b0398860b4a3b8b6e3d9d264116162
Deconinck Damien <ddeconinck@auchan.net>
2018-04-22 12:26:38 +0200

    Add important news

--- src/AppBundle/Form/Type/NewsType.php
+++ src/AppBundle/Form/Type/NewsType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -20,6 +21,14 @@ class NewsType extends AbstractType
                     'class' => 'ckeditor'
                 )
             ))
+            ->add('important', ChoiceType::class, [
+                'label' => 'Cette news est importante',
+                'expanded' => true,
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+            ])
             ->add('submit', SubmitType::class, array(
                 'label' => 'Valider',
                 'attr' => array('class' => 'btn btn-info btn-block')
